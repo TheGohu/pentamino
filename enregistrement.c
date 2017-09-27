9fb939064ecd6f43e16b7c33949fbaa07ce3d295
TheGohu <31927183+TheGohu@users.noreply.github.com>
2017-09-27 08:47:04 +0200

    Create enregistrement.c

---
+++ enregistrement.c
@@ -0,0 +1,23 @@
+<stdbool.h>
+
+typedef 
+struct piece
+{
+  char nom;
+  int largeur;
+  int hauteur;
+  bool pos_n;
+  bool pos_e;
+  int piece[5][5]; // tableau pour la forme des pieces
+}
+
+typedef 
+struct grille
+{
+  char nom;
+  int nb_l;  // nb de lignes
+  int nb_c;  // nb de colonnes
+  int x;     //coord x 
+  int y;     // coord y 
+  bool remplie;  // bool vrai si remplie 
+}
